6d1213bf536cf665e4264091b65fe21c3e5e70b0
Ratul Saha <hello@ratulsaha.com>
2020-05-03 22:00:20 +0800

    finished for screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import HorseIcon from "./images/horse.svg";
 
 import Content from "./content";
 
@@ -8,7 +9,14 @@ function App() {
     return (
         <div className="app">
             <header className="app__header">
-                <h1 className="app__header__title">A Betting App on Tezos</h1>
+                <h1 className="app__header__title">
+                    <img
+                        className="app__header__img"
+                        alt=""
+                        src={HorseIcon}
+                    ></img>{" "}
+                    A Betting App on Tezos
+                </h1>
             </header>
             <Content />
             <footer className="app__footer">
--- src/content.tsx
+++ src/content.tsx
@@ -1,14 +1,205 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Tezos } from "@taquito/taquito";
+import { InMemorySigner } from "@taquito/signer";
+
+import cogoToast from "cogo-toast";
 
 import "./content.css";
 
+type HorseType = {
+    horseId: String;
+    horseName: String;
+};
+
+type BetType = {
+    [sender: string]: { [horseId: string]: number };
+};
+
 const Content: React.FC = () => {
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const [totalBalance, setTotalBalance] = useState<undefined | number>(
+        undefined
+    );
+    const [track, setTrack] = useState<undefined | string>(undefined);
+    const [horses, setHorses] = useState<undefined | HorseType[]>(undefined);
+    const [bets, setBets] = useState<undefined | BetType[]>(undefined);
+
+    const [addHorseLoading, setAddHorseLoading] = useState(false);
+    const [runRaceLoading, setRunRaceLoading] = useState(false);
+    const [placeBetLoading, setPlaceBetLoading] = useState(false);
+
+    const contractId = "KT1R1LpgrWpDs6rSAwLDXyyKPtbGB9XBpRQB";
+    Tezos.setProvider({
+        rpc: "https://api.tez.ie/rpc/carthagenet",
+        signer: new InMemorySigner(
+            "edskS6rCh5wPWUd2D2vPRDmxnQppAr1V1e4MksLjQh1YgecFv4eYUaEYkWKFP6nYQJtopN9JT8Ms2vjoCRA4J2cUfRSe84HwZv"
+        ),
+    });
+    const getTotalBalance = () => {
+        Tezos.tz
+            .getBalance(contractId)
+            .then((balance) => setTotalBalance(balance.toNumber() / 1000000))
+            .catch((error) => console.log(JSON.stringify(error)));
+    };
+    const getContractDetails = async () => {
+        const contract = await Tezos.contract.at(contractId);
+        const storage: any = await contract.storage();
+        console.log(storage);
+        setTrack(storage?.track);
+        setHorses(storage?.horses);
+    };
+    const addHorse = async () => {
+        Tezos.contract
+            .at(contractId)
+            .then((contract) => {
+                cogoToast.info("Adding horse");
+                setAddHorseLoading(true);
+                return contract.methods
+                    .addHorse(1, "Test horse", "426c616168")
+                    .send();
+            })
+            .then((op) => {
+                cogoToast.info("Waiting to be confirmed");
+                return op.confirmation(3).then(() => op.hash);
+            })
+            .then((hash) => {
+                setAddHorseLoading(false);
+                cogoToast.success("Successfully added the horse!");
+            })
+            .catch((error) => {
+                setAddHorseLoading(false);
+                console.log(JSON.stringify(error));
+                cogoToast.error(
+                    `Could not add horse. Error:${JSON.stringify(error)}`
+                );
+            });
+    };
+    useEffect(() => {
+        // Tezos.setProvider({
+        //     rpc: "https://api.tez.ie/rpc/carthagenet",
+        //     // signer: new InMemorySigner(
+        //     // "edskS6rCh5wPWUd2D2vPRDmxnQppAr1V1e4MksLjQh1YgecFv4eYUaEYkWKFP6nYQJtopN9JT8Ms2vjoCRA4J2cUfRSe84HwZv"
+        //     // ),
+        // });
+        // const interval = setInterval(() => {
+        //     setCurrentTime(new Date());
+        // }, 1000);
+        getTotalBalance();
+        getContractDetails();
+        // return () => clearInterval(interval);
+    }, []);
     return (
         <div className="content">
             <div className="content__column">
                 <div className="content__item">
                     <div className="content__item__title">Track</div>
-                    <div className="content__item__content">fast</div>
+                    <div className="content__item__content">
+                        {track !== undefined ? `${track}` : ""}
+                    </div>
+                </div>
+                <div className="content__item">
+                    <div className="content__item__title">Total pool</div>
+                    <div className="content__item__content">
+                        {totalBalance !== undefined ? `${totalBalance}` : ""}
+                    </div>
+                </div>
+                <div className="content__item">
+                    <div className="content__item__title">Time of day</div>
+                    <div className="content__item__content">
+                        {currentTime.getHours()}:
+                        {currentTime.getMinutes() < 10 ? "0" : ""}
+                        {currentTime.getMinutes()}:
+                        {currentTime.getSeconds() < 10 ? "0" : ""}
+                        {currentTime.getSeconds()}
+                    </div>
+                </div>
+            </div>
+            <div className="content__column">
+                <div className="content__item">
+                    <div className="content__item__title-wrapper">
+                        <div className="content__item__title content__item__title--horses">
+                            Horses
+                        </div>
+                        <div className="content__item__title content__item__title--bets">
+                            Bets
+                        </div>
+                    </div>
+                    <div className="content__item__content-wrapper">
+                        <div className="content__item__content content__item__content--horses">
+                            {"Pickle Rake"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"4"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"1"}
+                        </div>
+                    </div>
+                    <div className="content__item__content-wrapper">
+                        <div className="content__item__content content__item__content--horses">
+                            {"Seabiscuit"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"3"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"2"}
+                        </div>
+                    </div>
+                    <div className="content__item__content-wrapper">
+                        <div className="content__item__content content__item__content--horses">
+                            {"Blaze"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"2"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"1"}
+                        </div>
+                    </div>
+                    <div className="content__item__content-wrapper">
+                        <div className="content__item__content content__item__content--horses">
+                            {"Shadowfax"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"4"}
+                        </div>
+                        <div className="content__item__content content__item__content--bets">
+                            {"1"}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="content__column content__column--action">
+                <div className="content__item">
+                    <button
+                        className="btn btn--admin"
+                        onClick={() => addHorse()}
+                    >
+                        {addHorseLoading ? (
+                            <span className="loadingSpinner"></span>
+                        ) : (
+                            "Add horse"
+                        )}
+                    </button>
+                </div>
+                <div className="content__item">
+                    <button className="btn btn--admin">
+                        {runRaceLoading ? (
+                            <span className="loadingSpinner"></span>
+                        ) : (
+                            "Run race"
+                        )}
+                    </button>
+                </div>
+                <div className="content__item">
+                    <button className="btn btn">
+                        {placeBetLoading ? (
+                            <span className="loadingSpinner"></span>
+                        ) : (
+                            "Place bet"
+                        )}
+                    </button>
                 </div>
             </div>
         </div>
